81bd67e31383e0eb7f4eaff933560bdbb125cb11
Rafael Cruz <rafaelcruz@Rafaels-MacBook-Pro.local>
2021-04-23 15:20:50 -0300

    ssl false

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,13 +13,16 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        ssl: false,
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
+      //   ssl: {
+      //     rejectUnauthorized: false,
+      //   },
+      // },
+      // options: {
+      //   ssl: true,
+      // },
     },
   },
 });
